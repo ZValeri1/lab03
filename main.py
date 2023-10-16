98d2c87768f3b18528c8f80b47a6b3f693ce21ce
ZValeri11 <valerkaluchnikov@yandex.ru>
2023-10-16 16:43:20 +0500

    bubble1

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-def bubble_sort(numbers):
+def bubble_sort(numbers, direction):
     n = len(numbers)
     # Проходим по всем парам элементов списка
     for i in range(n - 1):
         for j in range(n - i - 1):
-            if numbers[j] > numbers[j + 1]:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+            # В зависимости от направления сортировки
+            if direction == "возрастанию":
+                # Если текущий элемент больше следующего, меняем их местами
+                if numbers[j] > numbers[j + 1]:
+                    numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+            elif direction == "убыванию":
+                # Если текущий элемент меньше следующего, меняем их местами
+                if numbers[j] < numbers[j + 1]:
+                    numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
 # Запрашиваем у пользователя количество чисел
 n = int(input("Введите количество чисел: "))
@@ -15,8 +22,11 @@ for i in range(n):
     number = int(input("Введите число: "))
     numbers.append(number)
 
-# Сортируем числа с помощью сортировки пузырьком
-bubble_sort(numbers)
+# Запрашиваем у пользователя направление сортировки
+direction = input("Введите направление сортировки (возрастание/убывание): ")
+
+# Сортируем числа с помощью сортировки пузырьком и заданного направления
+bubble_sort(numbers, direction)
 
 # Выводим отсортированный список на экран
 print("Отсортированный список:")
